8072889d6a818d1f76c929f6048479adf6865151
hellowutong <hellowutong@126.com>
2016-10-12 09:56:08 +0800

    为webpack.config.js 添加注解

--- src/main/webapp/webpack.config.js
+++ src/main/webapp/webpack.config.js
@@ -3,7 +3,9 @@
  */
 var webpack = require('webpack');
 module.exports={
+		//页面入口文件配置
 		entry:__dirname+"/src/js/index.js", //"__driname"项目目录 唯一入库文件
+		//入口文件输出配置
 		output:{
 			path:__dirname+"/assets/js",
 			filename:'index.js',
@@ -16,7 +18,9 @@ module.exports={
 			color:true
 		},
 		module:{
+			//加载器配置
 			loaders:[
+			    //.css 文件使用 style-loader 和 css-loader 来处理
 				{
 					test:/\.css$/,
 					loader:'style-loader!css-loader'
@@ -27,7 +31,9 @@ module.exports={
 				},{
 					test:'/\.json$/',
 					loader:'json'
-				},{
+				},
+				 //.js 文件使用 jsx-loader 来编译处理
+				{
 					test:/\.js$/,
 					exclude:/node_modules/,//被排除的文件夹
 					loader:'babel',
@@ -38,8 +44,14 @@ module.exports={
 				}
 			]
 		},
+		//插件项
 		plugin:[
 			new webpack.HotModuleReplacementPlugin()
-		]
+		],
+		 //其它解决方案配置
+		resolve:{
+			
+		}
+		
 		
 }
